94f3a23ea042b2dfbde23b42aaa9489d89aeb40d
odnodn <odn@gmx.de>
2020-07-23 15:27:28 +0200

    treegrid 3 + sparklines added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,25 @@ import { Component } from '@angular/core';
 import { sampleData } from './datasource';
 import { projectData } from './datasource2';
 import {PageSettingsModel, SortSettingsModel } from '@syncfusion/ej2-angular-treegrid';
+import { textdata, getSparkData } from './datasource3';
+import { TreeGridComponent,ResizeService} from '@syncfusion/ej2-angular-treegrid';
+import { EmitType } from '@syncfusion/ej2-base';
+import { Sparkline, ISparklineLoadEventArgs, SparklineTheme } from '@syncfusion/ej2-charts';
+import { RowDataBoundEventArgs, getObject } from '@syncfusion/ej2-grids';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+   providers: [ResizeService ]
 })
 export class AppComponent  {
   name = 'Angular';
 
   public data: Object[];
   public data2: Object[];
+  public dataSpark: Object[] = [];
+  public sparkData: Object[] = [];
   public sortSettings: SortSettingsModel;
   public pageSettings: PageSettingsModel;
 
@@ -22,5 +30,12 @@ export class AppComponent  {
         this.sortSettings = { columns: [{ field: 'taskName', direction: 'Ascending' }, { field: 'taskID', direction: 'Descending' }]  };
         this.pageSettings = { pageSize: 6 };
         this.data2 = projectData;
+
+        this.dataSpark = textdata;
+        this.sparkData = getSparkData('line' + getObject('EmployeeID',textdata[0]));
+    }
+
+        getSparkLine (val: number) {
+        return getSparkData('line', val);
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
 import { PageService, SortService, FilterService } from '@syncfusion/ej2-angular-treegrid';
+import { SparklineAllModule } from '@syncfusion/ej2-angular-charts';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, TreeGridModule ],
+  imports:      [ BrowserModule, FormsModule, TreeGridModule, SparklineAllModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
-  providers:    [ PageService, SortService, FilterService ]
+  providers:    [ PageService, SortService, FilterService, SparklineAllModule ]
 })
 export class AppModule { }
